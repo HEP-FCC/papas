cf34200061583722f0de37489e279b8a99845768
Alice <Alice.Robson@cern.ch>
2016-01-14 09:06:14 +0100

    photon simulation developments

---
+++ pfobjects/particledata.h
@@ -0,0 +1,13 @@
+//
+//  particledata.h
+//  fastsim
+//
+//  Created by Alice Robson on 13/01/16.
+//
+//
+
+#ifndef particledata_h
+#define particledata_h
+
+
+#endif /* particledata_h */
---
+++ utility/enums.h
@@ -0,0 +1,13 @@
+//
+//  Header.h
+//  fastsim
+//
+//  Created by Alice Robson on 12/01/16.
+//
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+#endif /* Header_h */
