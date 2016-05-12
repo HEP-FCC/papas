6a73b7b1221cdb12aa78980e3aab72ab8fb665a6
Alice <alice.robson@cern.ch>
2016-05-12 14:29:37 +0200

    added Definitions.h

---
+++ datatypes/datatypes/Definitions.h
@@ -0,0 +1,29 @@
+//
+//  Definitions.h
+//  papas
+//
+//  Created by Alice Robson on 12/05/16.
+//
+//
+
+#ifndef Definitions_h
+#define Definitions_h
+#include "enummanager.h"
+
+
+
+namespace papas {
+  enum Position {kVertex,  kEcalIn, kEcalOut, kEcalDecay, kHcalIn, kHcalOut};
+  enum XLayer { kNone, kTracker, kEcal, kHcal, kField};
+
+  //ENUM_WITH_STRINGS(Layer, kVertex, kTracker, kEcal, kEcalIn, kEcalOut, kEcalDecay, kHcal, kHcalIn, kHcalOut, kField);
+
+}
+
+namespace fastsim {
+  
+  ENUM_WITH_STRINGS(enumSubtype , NONE, RAW, MERGED, SMEARED)
+  ENUM_WITH_STRINGS(enumSource , NONE, SIMULATION, RECONSTRUCTION, OBSERVED)
+}
+
+#endif /* Definitions_h */
