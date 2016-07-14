31a7b0710900465663fcdafb16bfe82f4f83f444
Alice <alice.robson@cern.ch>
2016-07-14 13:08:02 +0200

    Ctest now running under lxplus

--- init.sh
+++ init.sh
@@ -1,4 +1,3 @@
-
 platform='unknown'
 sw_afs=0
 unamestr=`uname`
@@ -6,13 +5,14 @@ unamestr=`uname`
 export LCGPATH=/afs/cern.ch/sw/lcg/views/LCG_83/x86_64-slc6-gcc49-opt
 #if [ -z ${FCCPAPASCPP+x} ]; then
     unset $FCCPAPASCPP
-    echo "FCCPAPASCPP is unset, setting to $FCCPAPASCPP";
+    echo "FCCPAPASCPP is unset, setting to $PWD/install";
     export FCCPAPASCPP=$PWD/install
     export PATH=$FCCPAPASCPP/bin:$PATH    
 #fi
 
 if [[ "$unamestr" == 'Linux' ]]; then
-    export LD_LIBRARY_PATH=$FCCPAPAS/lib:$LD_LIBRARY_PATH
+    platform='Linux'
+    export LD_LIBRARY_PATH=$FCCPAPASCPP/lib:$LD_LIBRARY_PATH
 elif [[ "$unamestr" == 'Darwin' ]]; then
     platform='Darwin'
     export DYLD_LIBRARY_PATH=$FCCPAPASCPP/lib:$DYLD_LIBRARY_PATH
