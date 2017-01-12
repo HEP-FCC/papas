72cd21b2523ba5d0eaf16f25bae2912de8555d93
Alice <alice.robson@cern.ch>
2017-01-12 14:19:52 +0100

    added papasmanagertester

---
+++ papas/reconstruction/src/PapasManagerTester.cpp
@@ -0,0 +1,44 @@
+//
+//  PapasManagerTester.cpp
+//  papas
+//
+//  Created by Alice Robson on 12/01/17.
+//
+//
+
+#include "papas/reconstruction/PapasManagerTester.h"
+
+namespace papas {
+  
+  
+  PapasManagerTester::PapasManagerTester(Detector& detector) :  PapasManager(detector) {}
+
+Simulator PapasManagerTester::setSimulator(const Particles& particles) {
+  
+  // create empty collections that will be passed to simulator to fill
+  // the new collection is to be a concrete class owned by the PapasManger
+  // and stored in a list of collections.
+  // The collection can then be passed to the Simulator and concrete objects
+  // stored in the collection
+  // create empty collections that will be passed to simulator to fill
+  // the new collection is to be a concrete class owned by the PapasManger
+  // and stored in a list of collections.
+  // The collection can then be passed to the Simulator and concrete objects
+  // stored in the collection
+  auto& ecalClusters = createClusters();
+  auto& hcalClusters = createClusters();
+  auto& smearedEcalClusters = createClusters();
+  auto& smearedHcalClusters = createClusters();
+  auto& tracks = createTracks();
+  auto& smearedTracks = createTracks();
+  auto& history = createHistory();
+  auto& simParticles = createParticles();
+  
+  // run the simulator which will fill the above objects
+  auto simulator = Simulator(m_papasEvent, particles, m_detector, ecalClusters, hcalClusters, smearedEcalClusters,
+                             smearedHcalClusters, tracks, smearedTracks, simParticles, history);
+  return simulator;
+  
+}
+
+} //end namespace papas
