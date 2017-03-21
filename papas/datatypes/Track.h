2959dc019b19cc2183fa1db16e03671df5726e6e
Alice <alice.robson@cern.ch>
2017-03-21 14:32:09 +0100

    tidy track class

--- papas/datatypes/Track.h
+++ papas/datatypes/Track.h
@@ -24,21 +24,16 @@ public:
    @param[in] the subtype of the track used in creating the unique id
    */
   Track(const TVector3& p3, double charge, const Path::Ptr path, unsigned int index, char subtype = 'u');
-  double pt() const { return m_p3.Perp(); } ///<momentum
   double energy() const { return m_p3.Mag(); } ///<energy
-  double eta() const { return m_p3.Eta(); }
   double charge() const { return m_charge; }
   IdType id() const { return m_uniqueId; } ///<unique identifier
   const TVector3& p3() const { return m_p3; } /// momentum
-  double theta() const { return M_PI / 2. - m_p3.Theta(); }
   const Path::Ptr path() const { return m_path; }
   void setEnergy(double energy);
   void setSize(double value);
-  static double s_maxenergy;  // AJR is this in the right place
   std::string info() const; ///< string representation of track
 protected:
   IdType m_uniqueId; ///< Unique identifier of track
-  double m_pt; ///< momentum
   TVector3 m_p3;  ///< momentum in 3D space (px, py, pz)
   double m_charge; ///< Charge of associated particle
   const Path::Ptr m_path;  ///< pointer to path (not owned by track)
